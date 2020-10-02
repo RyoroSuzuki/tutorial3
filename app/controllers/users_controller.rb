e5588f0569e2c6f4bf4bb301ac610d16e09840a8
RyoroSuzuki <ryoro@kitchhike.com>
2020-10-02 15:20:47 +0900

    user signup function halfly completed without integration test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "アカウントの作成（仮）完了したよ"
+      redirect_to user_path(@user.id)
+
+    else
+      render "new"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
