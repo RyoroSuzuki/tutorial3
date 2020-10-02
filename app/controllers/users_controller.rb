71f6bccbc77981716e214efb2794b484790f1068
RyoroSuzuki <ryoro@kitchhike.com>
2020-10-02 20:18:20 +0900

    basic-login function deployed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "アカウントの作成（仮）完了したよ"
       redirect_to user_path(@user.id)
 
