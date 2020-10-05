ce955bf4c8d422a9a92f7c66ca024956f3c5f89c
RyoroSuzuki <ryoro@kitchhike.com>
2020-10-05 14:41:45 +0900

    認可機能deployed & user REST page completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+
   def new
     @user = User.new
   end
@@ -20,9 +30,46 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "profile updated"
+      render "edit"
+    else
+      render "edit"
+    end
+  end
+
+    def destroy
+      User.find_by(id:params[:id]).destroy
+      flash[:success] = "消したよユーザー"
+      redirect_to users_path
+    end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてね"
+        redirect_to login_path
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless correct_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
     end
 
 end
