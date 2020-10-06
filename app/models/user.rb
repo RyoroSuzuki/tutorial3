70719403fb7c988519de39e69ad3bbc1ede11163
RyoroSuzuki <ryoro@kitchhike.com>
2020-10-06 15:40:48 +0900

    password reset function deployed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates :name,  presence: true, length: { maximum: 50 }
@@ -60,6 +60,25 @@ class User < ApplicationRecord
      def send_activation_email
         UserMailer.account_activation(self).deliver_now
      end
+
+     #パスワードリセットトークンを発行する
+     def create_reset_digest
+        self.reset_token = User.new_token
+        digest = User.digest(reset_token)
+        self.update_attribute(:reset_digest, digest)
+        self.update_attribute(:reset_sent_at, Time.zone.now)
+     end
+
+     #パスワードリセット用のメールを送る
+     def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+     end
+
+    # パスワード再設定の期限が切れている場合はtrueを返す
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
+
 
   end
 
